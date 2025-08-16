b397a00acec5435e62b55753312e97218f35c150
return-misaPuding <70533713+return-misaPuding@users.noreply.github.com>
2025-08-17 01:10:56 +0200

    add basic functions for manipulating the queue

---
+++ api.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+import sqlite3 as sq3
+import time
+#app = FastAPI()
+database = sq3.connect("queue.db")
+c = database.cursor()
+c.execute('''
+    create table if not exists queue (
+          ID integer primary key not null,
+          content text,
+          rel_pos float,
+          timestamp integer)
+''')
+database.commit()
+c.close()
+DB_INSERT = "insert into queue (ID, content, rel_pos, timestamp) values"
+c = database.cursor()
+c.execute("select * from queue")
+queueLen = len(c.fetchall())
+database.commit()
+c.close()
+def queue_write(content):
+    global queueLen
+    c = database.cursor()
+    c.execute(f"{DB_INSERT} {(queueLen, str(content), 1, round(time.time()))}")
+    #tuple to string formatting looks like this should work?
+    database.commit()
+    c.close()
+    queueLen += 1
+
+def queue_pop(pos):
+    c = database.cursor()
+    c.execute(f"select * from queue where ID = {pos}")
+    res = str(c.fetchone()[1])
+    c.execute(f"delete * from queue where ID = {pos}")
+    for i in range(queueLen-pos):
+        c.execute(f"update queue set ID = {pos+i} where ID = {pos+i+1}")
+        #shift all entries above pos down by 1
+    queueLen -= 1
+    database.commit()
+    c.close()
+    return res
