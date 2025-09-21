dc354987a026f773c678eb413f2ba5119d51a175
strawberry-p <strawberry@example.com>
2025-09-21 17:09:31 +0200

    added some endpoint documentation

--- api_wrap.py
+++ api_wrap.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Path
+from typing import Annotated
 import api_core as a
 from pydantic import BaseModel
 app = FastAPI()
 WRITE_PROTECT = ("info_queue", "info_event")
+NAMETYPE = Annotated[str,Path(description="Name of the target queue table",example="queue")]
 class Write(BaseModel):
     key: str | None = ""
     content: str
@@ -28,8 +30,8 @@ def dir_pop(name: str,key: str, s_q: bool):
     else:
         raise HTTPException(403)
 
-@app.post("/api/write/{name}", status_code=201) #Created
-def write(name: str, key: Write):
+@app.post("/api/write/{name}", status_code=201,summary="Add an item to the target queue's table") #Created
+def write(name: NAMETYPE, key: Write):
     try:
         info = a.q_info(name)
     except IndexError: #q_info raises an IndexError if the fetchmany returns an empty array (no result)
@@ -42,16 +44,22 @@ def write(name: str, key: Write):
     else:
         raise HTTPException(403)
 
-@app.get("/api/stack_pop/{name}")
-def stack_pop(name: str, key: Key):
+@app.get("/api/stack_pop/{name}",
+         summary="Retrieve and pop the last item from the given queue/stack table",
+         response_description="Content of the retrieved item in plaintext. \"\" means the queue is empty.")
+def stack_pop(name: NAMETYPE, key: Key):
     return dir_pop(name, str(key.key), True)
 
-@app.get("/api/pop/{name}")
-def pop(name: str, key: Key):
+@app.get("/api/pop/{name}",
+         summary="Retrieve and pop the first item from the given queue table",
+         response_description="Content of the retrieved item in plaintext. \"\" means the queue is empty.")
+def pop(name: NAMETYPE, key: Key):
     return dir_pop(name, str(key.key), False)
 
-@app.get("/api/length/{name}")
-def length(name: str):
+@app.get("/api/length/{name}",
+         summary="Returns the length of the given queue table",
+         response_description="Returns the length in plaintext")
+def length(name: NAMETYPE):
     if a.san_bool(name):
         try:
             return a.length_get(name)
@@ -60,12 +68,15 @@ def length(name: str):
     else:
         raise HTTPException(400,detail="sanitise_name")
 
-class Create(BaseModel):
+class CreateQueue(BaseModel):
     write_secret: str | None = ""
     read_secret: str | None = ""
 
-@app.put("/api/manage/{name}")
-def new(name: str, secrets: Create, status_code=201):
+@app.put("/api/manage/{name}",status_code=201,
+         summary="Create a new queue table with the input write and read keys",
+         description="(the read and write keys are stored in plaintext in the server. it is recommended to use random strings as the read/write keys.)",
+         response_description="Returns the queue table's delete key")
+def new(name: NAMETYPE, secrets: CreateQueue):
     res = a.queue_new(name,str(secrets.write_secret),str(secrets.read_secret))
     if res[1] == "invalid_name":
         raise HTTPException(status_code=400,detail="invalid name")
@@ -74,8 +85,9 @@ def new(name: str, secrets: Create, status_code=201):
     else:
         return res[0] #delete_secret
     
-@app.delete("/api/manage/{name}",status_code=204)
-def delete(name: str, key: Key):
+@app.delete("/api/manage/{name}",status_code=204,
+            summary="Delete the given queue you have created using its corresponding delete key")
+def delete(name: NAMETYPE, key: Key):
     res = a.queue_delete(name,key.key)
     if res != "":
         raise HTTPException(403,detail=res)
@@ -84,8 +96,9 @@ class Keychange(BaseModel):
     key: str | None = ""
     target_content: str | None = ""
 
-@app.post("/api/change_write_key/{name}",status_code=204)
-def write_key_change(name: str, keychange: Keychange):
+@app.post("/api/change_write_key/{name}",status_code=204,
+          summary="Change a given queue's write key using its corresponding delete key")
+def write_key_change(name: NAMETYPE, keychange: Keychange):
     try:
         err = a.queue_change_keys(name,keychange.key,keychange.target_content,False)
     except IndexError:
@@ -96,8 +109,9 @@ def write_key_change(name: str, keychange: Keychange):
         else:
             raise HTTPException(403,detail=err)
         
-@app.post("/api/change_read_key/{name}",status_code=204)
-def read_key_change(name: str, keychange: Keychange):
+@app.post("/api/change_read_key/{name}",status_code=204,
+          summary="Change a given queue's write key using its corresponding delete key")
+def read_key_change(name: NAMETYPE, keychange: Keychange):
     try:
         err = a.queue_change_keys(name,keychange.key,keychange.target_content,True)
     except IndexError:
